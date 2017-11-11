2d28da1c777d19644c5bfdf4ed215838b7053f04
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-11-11 16:28:01 +0900

    don't use test_set for validation

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -69,7 +69,7 @@ history = model.fit(x_train, y_train,
                     batch_size=params["batch_size"],
                     epochs=params["epochs"],
                     verbose=1,
-                    validation_data=(x_test, y_test),
+                    validation_split=0.1,
                     callbacks=[early_stopping])
 score = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
